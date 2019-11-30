f849ad567fb2b76cbe8dd94ee96bb0f27fc4b7b2
Daniel Robinson <danielrobinson095@gmail.com>
2019-11-29 21:08:24 -0500

    adding contact me form

--- scripts/site.js
+++ scripts/site.js
@@ -1,5 +1,6 @@
 var isHidden = true;
 
+
 $(document).ready(function() {
     new TypeIt('#intro-verb', {
         strings: ['Passionate', 'Determined', 'Hard-Working', 'Dedicated'],
@@ -9,6 +10,9 @@ $(document).ready(function() {
         breakLines: false,
         loopDelay: 3000
     }).go();
+
+    registerContactForm();
+
 });
 
 
@@ -44,3 +48,46 @@ jQuery.expr.filters.offscreen = function (el) {
         (rect.x > window.innerWidth || rect.y > window.innerHeight)
     );
 };
+
+function registerContactForm() {
+    var form     = document.getElementById("contact-form");
+    var button   = document.getElementById("form-submit-btn");
+    var status   = document.getElementById("my-form-status");
+
+    function success() {
+        $('.modal').modal('hide');
+        form.reset();        
+        $('#notification-title').text('Message sent');
+        $('#notification-message').text('Thanks for reaching out, I will be in touch soon!');
+        $('.toast').toast('show');
+    }
+
+    function error() {
+        $('#notification-title').text('Oh no.');
+        $('#notification-message').text('Something went wrong, please try again.');
+        $('.toast').toast('show');
+    }
+
+    form.addEventListener("submit", function (ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        post(data, success, error);
+    });
+}
+
+function post(data, success, error) {
+    const method = "POST";
+    const url = "https://formspree.io/xjvvlqkk";
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+          success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
